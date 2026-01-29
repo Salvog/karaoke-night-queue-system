6bf32c762d8018f769e24fe77dfe0acce8c233ba
Salvog <38378105+Salvog@users.noreply.github.com>
2026-01-29 23:18:55 +0100

    Register core Laravel service providers

--- config/app.php
+++ config/app.php
@@ -12,6 +12,18 @@ return [
     'cipher' => 'AES-256-CBC',
 
     'providers' => [
+        Illuminate\Auth\AuthServiceProvider::class,
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+        Illuminate\Log\LogServiceProvider::class,
+        Illuminate\Pagination\PaginationServiceProvider::class,
+        Illuminate\Routing\RoutingServiceProvider::class,
+        Illuminate\Session\SessionServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class,
+        Illuminate\Validation\ValidationServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
+
         App\Providers\AppServiceProvider::class,
     ],
 ];
